f7738522f2803e5b5e58bca619504253450d1e21
Kasper1999 <sasha.kasper12@mail.ru>
2018-10-03 19:14:38 +0300

    first try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let result = 0;
+  for(let i = base; number / i >=1; i *= base){
+    result += Math.floor(number / i)
+  }
+
+return (result);
 }
