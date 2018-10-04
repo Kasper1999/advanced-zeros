d15cf5bb0d9d01de885e5f24565172b6106af3e1
Kasper1999 <sasha.kasper12@mail.ru>
2018-10-04 18:44:51 +0300

    advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,9 +1,57 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
-  let result = 0;
-  for(let i = base; number / i >=1; i *= base){
-    result += Math.floor(number / i)
+  
+  var mnoz=[]; var i=2, j=0;
+  
+  while(base!=1){
+  if(base%i==0) { 
+      mnoz[j]=i; base= base/i; j++
+  }
+  else if(base%i!=0) i++;
+  }
+  
+  var iskl=[]; var arrp=[];
+  var j=0,l=0;
+  var er=0;
+  var povt=0;
+  
+  for (var i=0;i<mnoz.length;i++){ 
+    for(var y=0;y<iskl.length;y++){
+      if(iskl[y]==mnoz[i]) er++;
+      }
+      if(!er) {
+      iskl[j]=mnoz[i];
+      j++;
+      }
+      if (er>0) er=0;
+    }
+  
+    for(var y=0;y<iskl.length;y++){
+      for (var i=0;i<mnoz.length;i++){
+        if(iskl[y]==mnoz[i]) povt++;
+      } arrp[l]=povt; l++; povt=0;
+    }
+
+  var degree = [];
+  for(var ii=0; ii<iskl.length; ii++){
+    degree[ii]=0;
   }
 
-return (result);
-}
+  
+  for(var ii=0; ii<iskl.length; ii++){
+    for(var li = iskl[ii]; number / li >=1; li *= iskl[ii]){
+      degree[ii] += Math.floor(number / li)
+    }
+  }
+
+  mnoz=[];
+  for(var ii=0; ii<iskl.length; ii++){
+    mnoz[ii]=Math.trunc(degree[ii]/arrp[ii])
+  }
+
+  var min=mnoz[0];
+  for(var ii=0; ii<mnoz.length; ii++){
+    if(mnoz[ii]<min) min=mnoz[ii];
+  }
+  return (min);
+  }
